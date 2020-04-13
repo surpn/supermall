0a9c83d823e0ce3ebded25981585db120b57f169
surpn <422563052@qq.com>
2020-04-13 20:41:11 +0800

    提交icon

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,25 @@
 module.exports = {
-    devServer:{
-        disableHostCheck: true,
-    },
-    configureWebpack:{
-        resolve:{
-            alias: {
-                '@': 'src',
-                'common': '@/common',
-                'components': '@/components',
-                'network': '@/network',
-                'views': '@/views'
-            }
-        }
+  devServer:{
+      disableHostCheck: true,
+  },
+  configureWebpack:{
+      resolve:{
+          alias: {
+              '@': 'src',
+              'common': '@/common',
+              'components': '@/components',
+              'network': '@/network',
+              'views': '@/views'
+          }
+      }
+  },
+  pwa: {
+    iconPaths: {
+      favicon32: '@/assets/favicon.ico',
+      favicon16: 'f@/assets/favicon.ico',
+      appleTouchIcon: '@/assets/favicon.ico',
+      maskIcon: '@/assets/favicon.ico',
+      msTileImage: '@/assets/favicon.ico'
     }
-
-
-
+  }
 }
